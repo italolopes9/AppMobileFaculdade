dd32e86554c7972b3a08535b6fd928eeb6fd6b5b
italolopes9 <it-alo19@hotmail.com>
2020-04-06 15:10:56 -0300

    Alterações da atividade 9

--- app/src/main/java/com/projeto/appmobile/MainActivity.java
+++ app/src/main/java/com/projeto/appmobile/MainActivity.java
@@ -1,5 +1,6 @@
 package com.projeto.appmobile;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,13 +12,14 @@ import android.widget.TextView;
 import com.google.android.material.textfield.TextInputEditText;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private EditText campoNome;
     private TextInputEditText campoTelefone;
-    private TextView textoResultado;
+
     private CheckBox sms;
     private CheckBox email;
     private CheckBox fone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,28 +28,37 @@ public class MainActivity extends AppCompatActivity {
 
         campoNome = findViewById(R.id.editNome);
         campoTelefone = findViewById(R.id.editTelefone);
-        textoResultado = findViewById(R.id.textResultado);
 
         sms = (CheckBox) findViewById(R.id.sms);
         email = (CheckBox) findViewById(R.id.email);
         fone = (CheckBox) findViewById(R.id.fone);
-
-
     }
 
-    public void  enviar(View view) {
-
+    @Override
+    public void onClick(View view) {
         String nome = campoNome.getText().toString();
         String telefone = campoTelefone.getText().toString();
 
-
-        textoResultado.setText("Nome: " + nome + "Telefone: " + telefone );
+        String mensagem = "Nome: " + nome + "\nTelefone: " + telefone + "\nA forma de contato marcada foi: ";
+        if (sms.isChecked())
+            mensagem += " SMS ";
+        if (email.isChecked())
+            mensagem += " Email ";
+        if (fone.isChecked())
+            mensagem += " Telefone ";
+        AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+        dlg.setMessage(mensagem);
+        dlg.setNeutralButton("OK", null);
+        dlg.show();
     }
+
 
     public void limpar(View view) {
         campoNome.setText("");
         campoTelefone.setText("");
     }
+
+
 
 
 
