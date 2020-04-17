7228a2b0751363468960aaf59f8c1e323f141551
italolopes9 <it-alo19@hotmail.com>
2020-04-17 15:32:56 -0300

    Atividade semana 11

--- app/src/main/java/com/projeto/appmobile/MainActivity.java
+++ app/src/main/java/com/projeto/appmobile/MainActivity.java
@@ -1,24 +1,29 @@
 package com.projeto.appmobile;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
     private EditText campoNome;
     private TextInputEditText campoTelefone;
+    private TextView textoResultado;
 
     private CheckBox sms;
     private CheckBox email;
     private CheckBox fone;
+
+    private RadioButton sexoMasculino;
+    private RadioButton sexoFeminino;
+
 
 
     @Override
@@ -28,30 +33,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         campoNome = findViewById(R.id.editNome);
         campoTelefone = findViewById(R.id.editTelefone);
+        textoResultado = findViewById(R.id.textResultado);
 
-        sms = (CheckBox) findViewById(R.id.sms);
-        email = (CheckBox) findViewById(R.id.email);
-        fone = (CheckBox) findViewById(R.id.fone);
+        sms = findViewById(R.id.sms);
+        email = findViewById(R.id.email);
+        fone =  findViewById(R.id.fone);
+
+        sexoMasculino = findViewById(R.id.radioMasculino);
+        sexoFeminino = findViewById(R.id.radioFeminino);
+
     }
 
-    @Override
-    public void onClick(View view) {
-        String nome = campoNome.getText().toString();
-        String telefone = campoTelefone.getText().toString();
+    public void checkbox() {
+        String texto = "";
 
-        String mensagem = "Nome: " + nome + "\nTelefone: " + telefone + "\nA forma de contato marcada foi: ";
-        if (sms.isChecked())
-            mensagem += " SMS ";
-        if (email.isChecked())
-            mensagem += " Email ";
-        if (fone.isChecked())
-            mensagem += " Telefone ";
-        AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-        dlg.setMessage(mensagem);
-        dlg.setNeutralButton("OK", null);
-        dlg.show();
+        if(sms.isChecked()){
+            texto = "SMS - ";
+        }
+        if(email.isChecked()){
+            texto = texto + "EMAIL - ";
+        }
+        if(fone.isChecked()){
+            texto = texto + "TELEFONE - ";
+        }
+
+        textoResultado.setText(texto);
     }
 
+    public void radioButton(){
+        if(sexoMasculino.isChecked()){
+            textoResultado.setText("Masculino");
+        } else if(sexoFeminino.isChecked()){
+            textoResultado.setText("Feminino");
+        }
+    }
+
+    public void enviar(View view) {
+        radioButton();
+
+
+//        checkbox();
+
+//        String nome =  campoNome.getText().toString();
+//        String telefone = campoTelefone.getText().toString();
+//
+//        textoResultado.setText("Nome: " + nome + "\nTelefone: " + telefone);
+
+
+    }
 
     public void limpar(View view) {
         campoNome.setText("");
